9e3d97215cac96f59fea5720382324284e7952e0
gkumar-pandey <gautamshekhar078@gmail.com>
2023-06-18 21:52:40 +0530

    update

--- src/pages/ArchivePage.jsx
+++ src/pages/ArchivePage.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import Container from "../components/Container";
+import { useHabit } from "../context/habit-context";
+import HabitTiles from "../components/HabitTiles";
 
 const ArchivePage = () => {
+  const {
+    habitState: { archiveHabits }
+  } = useHabit();
   return (
     <div>
       <Container>
         <h1 className=" text-4xl text-center  ">Archive Habits</h1>
+        <div className="flex flex-row flex-wrap">
+          {archiveHabits.map((item) => (
+            <div>
+              <HabitTiles {...item} />
+            </div>
+          ))}
+        </div>
       </Container>
     </div>
   );
